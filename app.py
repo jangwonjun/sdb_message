f47c79a575915f001128c52b716d0f70e0f21d1a
wonjunJang <nuriyooyoung@gmail.com>
2023-12-04 00:27:40 +0900

    modify1

--- app.py
+++ app.py
@@ -5,64 +5,48 @@ from env import SEND, FLASK_ENUM
 from tqdm import tqdm
 from modules.candidate import candidate_data
 from flask import Flask, request, redirect, render_template, url_for, session
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
 app = Flask(__name__, static_url_path='/static')
 
 sdb_system = candidate_data()
 
-student_phone_num_data = sdb_system.search_phone_num_student()[0]
-student_name_data = sdb_system.search_phone_num_student()[1]
-parents_phone_num_data = sdb_system.search_phone_num_parents()[0]
-send_data = sdb_system.search_phone_num_parents()[1]
+student_phone_num_data = sdb_system.search_phone_num_student()
+num_inf = sdb_system.send_student_num()
+parents_phone_num_data = sdb_system.search_phone_num_parents()
+send_data = sdb_system.send_student_message()
+
+print(len(student_phone_num_data)) #sdb db에서 이름과 대조하여 동일한 학생을 찾은 경우
+print(student_phone_num_data) #[학생전화번호,이름] 
+print(len(send_data)) #실제로 메세지를 보내야하는 인원의 수(즉, 정리하자면 3번 리스트에 정리한 인원)
 
 
-
-print(len(send_data))
-
-@app.route('/parents_message')
-def parents_send_message():
-    for i in range(len(send_data)):
-        parents_message = f'안녕하세요! {student_name_data[i]} 학부모님 수다방학원입니다. \n금일 수업 내용입니다. \n[진도] : {send_data[i][2]} \n[과제] : {send_data[i][3]} \n[과제 수행도] : {send_data[i][4]} \n[벌점] : {send_data[i][5]} \n[테스트] : {send_data[i][6]} \n[반평균] : {send_data[i][8]} \n[최고점] : {send_data[i][9]} \n[특이사항] : {send_data[i][10]}'
-        '''
-        data = {
-            'messages': [
-                {
-                    'to': parents_phone_num_data[i],
-                    'from': SEND.SENDNUMBER,
-                    'subject': '수다방학원',
-                    'text': parents_message
-                }
-            ]
-        }
-        
-        res = message.send_many(data)
-        print(f"{student_name_data[i]} 학부모에게 성공적으로 전송했습니다")
-        print(json.dumps(json.loads(res.text), indent=2, ensure_ascii=False))
-        '''
-    return "전송하였습니다."
-
-@app.route('/student_message')
+@app.route('/message_send')
 def student_send_message():
-    for i in range(len(send_data)):
-        student_message = f'안녕! 수다방이야 {student_name_data[i]} 반가워! \n[진도] : {send_data[i][2]} \n[과제] : {send_data[i][3]} \n[과제 수행도] : {send_data[i][4]} \n[벌점] : {send_data[i][5]} \n[테스트] : {send_data[i][6]} \n[반평균] : {send_data[i][8]} \n[최고점] : {send_data[i][9]} \n[특이사항] : {send_data[i][10]}'
-        '''
-        data = {
-            'messages': [
-                {
-                    'to': student_phone_num_data[i],
-                    'from': SEND.SENDNUMBER,
-                    'subject': '수다방학원',
-                    'text': student_message
-                }
-            ]
-        }
+    #여기서의 for문은 보내야할 인원이 아닌, 동일한 학생을 찾은 경우로 생각하고 진행해야지 올바름.
+    for i in range(len(student_phone_num_data)):
         
+        data = {
+                'messages': [
+                    {
+                        'to': student_phone_num_data[i][0],
+                        'from': SEND.SENDNUMBER,
+                        'subject': '수다방학원',
+                        'text': send_data[i][0]
+                    }
+                ]
+            }
         res = message.send_many(data)
-        print(f"{student_name_data[i]}에게 성공적으로 전송했습니다")
+        print(f"{send_data[i][1]}에게 성공적으로 전송했습니다")
         print(json.dumps(json.loads(res.text), indent=2, ensure_ascii=False))
-        '''
+
     return "전송하였습니다."
         
+@app.route('/main')
+def main():
+    return render_template('index.html')
+
     
 if __name__ == '__main__':
     app.run('0.0.0.0', debug=True, port=FLASK_ENUM.PORT)
--- modules/candidate.py
+++ modules/candidate.py
@@ -30,29 +30,70 @@ class candidate_data():
         doc = gc.open_by_url(spreadsheet_url)
 
         # 시트 선택하기
-        worksheet = doc.worksheet('수다방_문자서비스')
-
+        worksheet_1 = doc.worksheet('수다방_문자서비스')
+        worksheet_2 = doc.worksheet('전송인원정보')
+        self.worksheet_3 = doc.worksheet('전송메시지')
+        worksheet_4 = doc.worksheet('주관관리표')
+        
+        #num_inf는 출력할 학생수를 의미한다!
+        
+        self.num_inf = worksheet_2.acell('C5').value
+        
         # 범위(셀 위치 리스트) 가져오기
-        range_list = worksheet.range('A2:K5')
-
-        self.index_num = worksheet.row_count
+        range_list = worksheet_1.range('A2:M9')
+        
+        #상속처리하자!
+        self.index_num = worksheet_1.row_count
 
         # 범위에서 각 셀 값 가져오기
         for cell in range_list:
             message.append(cell.value)
 
+        #',' 단위로 나누기!
         for i in range(len(message)):
-            # '.'을 기준으로 끊고 빈 문자열 제거하여 리스트로 변환
-            message1.extend(list(filter(None, message[i].split('.'))))
+            message1.extend(list(filter(None, message[i].split(','))))
 
-        # 날짜의 위치 찾기
-        date_index = [i for i, item in enumerate(message1) if '/' in item]
+        interval = 13
+        inform_student = []
 
-        self.result = [message1[date_index[i-1]:date_index[i]] for i in range(1, len(date_index))]
+        for i in range(0,len(message1),interval):
+            temp_slice = message1[i:i+interval]
+            inform_student.append(list(filter(None,temp_slice)))
 
-        self.result.append(message1[date_index[-1]:])
+        #이름 : inform_student[0][2]
+        
+        #print(inform_student[5])
 
+       
+        for i in range(int(self.num_inf)):
+            message = f"안녕하세요! 어머니 수다방 수학학원 하웅진T 입니다. 금일 수업 진행상황 알려드립니다!\n[수업내용]\n\n{inform_student[i][4]}\n\n[교재]\n\n{inform_student[i][5]}"
+
+            self.worksheet_3.update(f'A{i+2}',inform_student[i][2])
+            self.worksheet_3.update(f'C{i+2}',message)
+            self.result.append([message, inform_student[i][2]])
+
+        #print(self.result[0])
+
+
+    def send_student_num(self):
+        #print("전송할 학생 숫자를 입력받고 넘김 ㅋ")
+        return self.num_inf
     
+    def send_student_message(self):
+        #무조건 전송해야할값들
+        #print("학생들의 멘트 넘김.")
+        self.data = []
+        for i in range(int(self.num_inf)):
+            send_message = self.worksheet_3.acell(f'C{i+2}').value
+            send_student_name = self.worksheet_3.acell(f'A{i+2}').value
+            #print(send_message,send_student_name)
+            print(i)
+            self.data.append([send_message,send_student_name])
+        
+        #print("출력할 데이터들",self.data)
+        return self.data
+
+
     def search_phone_num_student(self):
         with open('data/sdb_student_db.csv',encoding='utf-8') as f:
             sdb_db = f.read()
@@ -60,41 +101,43 @@ class candidate_data():
         data_list = [line.split(',') for line in sdb_db.split('\n')]
         data_list[0][0] = data_list[0][0].lstrip('\ufeff')
         
-        #print(data_list[0][0])
-        
-        for i in range(len(data_list)):
-            self.send_name_student.append(data_list[i][0])
-        
-        #print(self.send_name_student)
-        
+        #print(len(data_list))
+
         for p in range(len(data_list)):
-            if data_list[p][0] == self.result[p][1]:
-                #학생이름을 통해 학생 전화번호 찾기
-                #print(data_list[p][3])
-                self.send_num_student.append(data_list[p][3])
+            for i in range(int(self.num_inf)):
+                if data_list[p][0] == self.result[i][1]:
+                    #학생이름을 통해 학생 전화번호 찾기
+                    #print(data_list[p][3])
+                    #중복방지작업_1차안 마련해야함. 일단 진행하기
+
+                    #[학생전화번호, 이름] 2차원 배열로 저장
+                    self.send_num_student.append([data_list[p][3],data_list[p][0]])
             
-        print("TEST", self.send_name_student)
+        #print("TEST", self.send_num_student)
         
-        return self.send_num_student, self.send_name_student # 전화번호 데이터, 이름 데이터 반환
+        return self.send_num_student
     
     def search_phone_num_parents(self):
         with open('data/sdb_student_db.csv',encoding='utf-8') as f:
             sdb_db_parents = f.read()
             
         parents_data_list = [line.split(',') for line in sdb_db_parents.split('\n')]
+        parents_data_list[0][0] = parents_data_list[0][0].lstrip('\ufeff')
         
-        #print(data_list[0][0])
+        #print(parents_data_list)
         
-        for i in range(len(parents_data_list)):
-            if parents_data_list[i][0] == self.result[i][1]:
-                #학생이름을 통해 부모님 전화번호 찾기
-                #print(data_list[i][4])
-                self.send_num_parents.append(parents_data_list[i][4])
+        for p in range(len(parents_data_list)):
+            for i in range(int(self.num_inf)):
+                if parents_data_list[p][0] == self.result[i][1]:
+                    #학생이름을 통해 학생 전화번호 찾기
+                    
+                    #중복방지작업_1차안 마련해야함. 일단 진행하기
+
+                    #[학부모전화번호, 학생이름] 2차원 배열로 저장
+                    self.send_num_parents.append([parents_data_list[p][4],parents_data_list[p][0]])
             
-        #print(self.send_num_parents)
+        #print("TEST", self.send_num_parents)
         
-        return self.send_num_parents, self.result #  학부모 전화번호 데이터 반환
+        return self.send_num_parents
     
         
-
-
